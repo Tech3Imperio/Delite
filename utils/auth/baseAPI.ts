4e395c1efc9b3e91007bc432df0886bd917f36a4
Imperio Tech <imperiotech@PayaIshans-MacBook-Pro.local>
2025-05-19 11:25:26 +0530

    added backend url to baseAPI.ts

--- utils/auth/baseAPI.ts
+++ utils/auth/baseAPI.ts
@@ -1,6 +1,10 @@
 // utils/apiBase.ts
 import { Platform } from "react-native";
 export const getApiBaseUrl = () => {
-    if (Platform.OS === "android") return "http://10.0.2.2:8000";
-    return "http://localhost:8000";
+    if (__DEV__) {
+        if (Platform.OS === "android") return "http://10.0.2.2:8000";
+        return "http://localhost:8000";
+    } else {
+        return "https://delite-backend-3lhu.onrender.com"
+    }
 };
